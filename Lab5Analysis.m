a2a37bb5d7c243db2992e9594ab187010b1b707c
ZhangxiJesseFeng <zf1005@wildcats.unh.edu>
2017-11-30 23:06:38 -0500

    part 3 stuff

--- Lab5Analysis.m
+++ Lab5Analysis.m
@@ -10,11 +10,15 @@ KtachSens = 3; % V/kRPM
 
 Part2_a_EtOmega = Part2_a_Et/KtachSens; % [kRPM]
 
-Part2_a_fit = polyfit(Part2_a_EtOmega(2:end),Part2_a_MUT(2:end),1);
+Part2_a_fit = polyfit(Part2_a_EtOmega,Part2_a_MUT,1);
 KeCalc = Part2_a_fit(1); % 4.9817 [V/kRPM] - right on the money
 
+Part2_a_fitY = Part2_a_EtOmega*Part2_a_fit(1) + Part2_a_fit(2);
+
 figure(1);
-plot(Part2_a_EtOmega,Part2_a_MUT);
+hold on;
+plot(Part2_a_EtOmega,Part2_a_MUT,'O');
+plot(Part2_a_EtOmega,Part2_a_fitY,'--');
 xlabel('\omega (kRPM)','FontSize',12);
 ylabel('MUT Voltage (V)','FontSize',12);
 
@@ -47,52 +51,51 @@ tau = tau_calc*1000*(2*pi/60); % [s-rad]
 % ch. 0: Tach output ch. 1: ei after op-amp
 Part3_b_data = importdata('Data from Section 3 Step 7.lvm','\t',33);
 Part3_b_data = Part3_b_data.data;
-Part3_Time = Part3_b_data(:,1);
-Part3_TachV = Part3_b_data(:,2);
-Part3_InputV = Part3_b_data(:,3);
+Part3_b_Time = Part3_b_data(:,1);
+Part3_b_TachV = Part3_b_data(:,2);
+Part3_b_InputV = Part3_b_data(:,3);
 
 figure(2);
 hold on;
-plot(Part3_Time,Part3_TachV);
-plot(Part3_Time,Part3_InputV);
+plot(Part3_b_Time,Part3_b_TachV);
+plot(Part3_b_Time,Part3_b_InputV);
 xlabel('Time (s)','FontSize',12);
 ylabel('Voltages (V)','FontSize',12);
 legend('Location','best','Tachometer Output','Input Voltage e_a');
 
-Part3_Time = Part3_Time(501:2500);
-Part3_TachV = Part3_TachV(501:2500);
-Part3_InputV = Part3_InputV(501:2500);
+% data reconfiguration
+Part3_b_Time = Part3_b_Time(501:2500);
+Part3_b_TachV = Part3_b_TachV(501:2500);
+Part3_b_InputV = Part3_b_InputV(501:2500);
 
-Part3_TachV = smooth(smooth(Part3_TachV));
-Part3_InputV = smooth(smooth(Part3_InputV));
+Part3_b_TachV = smooth(smooth(Part3_b_TachV));
+Part3_b_InputV = smooth(smooth(Part3_b_InputV));
 
-Part3_TachV = Part3_TachV - mean(Part3_TachV(1:100));
+Part3_b_TachV = Part3_b_TachV - mean(Part3_b_TachV(1:100));
 
-TachV_FinalValue = mean(Part3_TachV(1000:end));
+Part3_b_FinalV = mean(Part3_b_TachV(1000:end));
+% 63.2% tau point
+Part3_b_TauV = 0.632*Part3_b_FinalV;
 
-TauV = 0.632*TachV_FinalValue;
-
-for i = 1:length(Part3_Time)
-    if (Part3_Time(i) > -0.006389)
-        startInd = i;
+for i = 1:length(Part3_b_Time)
+    if (Part3_b_Time(i) > -0.006389)
+        Part3_b_Time = Part3_b_Time - Part3_b_Time(i);
         break;
     end
 end
 
-Part3_Time = Part3_Time - Part3_Time(startInd);
-
-for i = 1:length(Part3_TachV)
-    if (Part3_TachV(i) > TauV)
-        tauInd = i;
-        tauTime = Part3_Time(tauInd);
+for i = 1:length(Part3_b_TachV)
+    if (Part3_b_TachV(i) > Part3_b_TauV)
+        Part3_b_tauInd = i;
+        Part3_b_tauTime = Part3_b_Time(i);
         break;
     end
 end
 
 figure(3);
 hold on;
-plot(Part3_Time,Part3_TachV);
-plot(Part3_Time(tauInd),Part3_TachV(tauInd),'O');
+plot(Part3_b_Time,Part3_b_TachV);
+plot(Part3_b_Time(Part3_b_tauInd),Part3_b_TachV(Part3_b_tauInd),'O');
 xlabel('Time (s)','FontSize',12);
 ylabel('Voltages (V)','FontSize',12);
 legend('Location','best','Tachometer Output','Tau Point');
@@ -101,9 +104,53 @@ legend('Location','best','Tachometer Output','Tau Point');
 % gain = KtKtach/(BR + KtKe)
 % Total step input amplitude: 16 V
 % Total step response final value: 8 V
-inputGain = Part3_InputV(1);
-stepGain = Part3_TachV(end);
+inputGain = Part3_b_InputV(1);
+stepGain = Part3_b_TachV(end);
 totalGain = stepGain/inputGain;
 
 Bcalc = 1/R*(Kt*KtachSens/totalGain - Kt*KeCalc);
+
+Jcalc = Part3_b_tauTime*(Bcalc*R + Kt*KeCalc)/R; % [Oz-in-s/kRPM]
+Jcalc = Jcalc/1000/(2*pi/60); % [oz-in-s^2/rad]
+
 %% Part 3 e)
+% Import data for disturbance
+Part3_e_data = importdata('Data from Section 3 Step 9.lvm','\t',33);
+Part3_e_time = Part3_e_data.data(:,1);
+Part3_e_tachV = Part3_e_data.data(:,2);
+Part3_e_inputV = Part3_e_data.data(:,3);
+
+Part3_e_tachV = smooth(smooth(Part3_e_tachV));
+
+for i = 1:length(Part3_e_time)
+    if (Part3_e_time(i) > 0.0221)
+        Part3_e_time = Part3_e_time - Part3_e_time(i);
+        break;
+    end
+end
+
+Part3_e_time = Part3_e_time(500:4000);
+Part3_e_tachV = Part3_e_tachV(500:4000);
+Part3_e_inputV = Part3_e_inputV(500:4000);
+
+Part3_e_stV = mean(Part3_e_tachV(1:300));
+Part3_e_endV = mean(Part3_e_tachV(3000:end));
+Part3_e_tauV = Part3_e_endV + (Part3_e_stV - Part3_e_endV)*0.368;
+
+for i = 1:length(Part3_e_tachV)
+    if (Part3_e_tachV(i) < Part3_e_tauV)
+        Part3_e_tauInd = i;
+        break;
+    end
+end
+
+Part3_e_tauTime = Part3_e_time(Part3_e_tauInd);
+Part3_e_tauVolt = Part3_e_tachV(Part3_e_tauInd);
+
+figure(4);
+hold on;
+plot(Part3_e_time,Part3_e_tachV);
+plot(Part3_e_time(Part3_e_tauInd),Part3_e_tachV(Part3_e_tauInd),'O');
+xlabel('Time (s)','FontSize',12);
+ylabel('Tachometer Voltage (V)','FontSize',12);
+legend('Location','best','Tach Output','Tau Point');
